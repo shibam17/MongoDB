7d11c025b2857888a6716a123112fa99c4a39840
shibam17 <shibam.dipu@gmail.com>
2022-08-04 20:08:27 +0530

    done

--- mongodbNodejs/app.js
+++ mongodbNodejs/app.js
@@ -80,3 +80,22 @@ app.delete('/books/:id', (req, res) =>{
     res.status(500).json({error: "not valid doc"})
   }
 })
+
+
+app.patch('/books/:id', (req, res) =>{
+  const updates = req.body
+ if (ObjectId.isValid(req.params.id)) {
+   db.collection("books")
+     .updateOne({ _id: ObjectId(req.params.id) }, {$set: updates})
+     .then((result) => {
+       res.status(200).json(result);
+     })
+     .catch((err) => {
+       res.status(500).json({ err: "not deleted" });
+     });
+ } else {
+   res.status(500).json({ error: "not valid doc" });
+ }
+
+
+})
