6f7fb043f706d502d33e9ff1a4b6fba1064f773b
shibam17 <shibam.dipu@gmail.com>
2022-08-04 18:54:05 +0530

    added

---
+++ mongodbNodejs/app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const { ObjectId } = require("mongodb");
+const { connectToDb, getDB } = require("./db");
+
+// init app and middleware
+
+const app = express();
+
+app.use(express.json()) //to use it in post request
+
+// db connection
+let db;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log("app listening to port 3000");
+    });
+    db = getDB();
+  }
+});
+
+//  routes
+
+app.get("/books", (req, res) => {
+  let books = [];
+
+  db.collection("books")
+    .find() //cursor toArray or forEach
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+        res.status(200).json(books)
+    })
+    .catch(() =>{
+        res.status(500).json({error: 'could not fetch the dociments'})
+    })
+//   res.json({ mssg: "welcome to api" });
+});
+
+
+app.get('/books/:id', (req, res) =>{
+    
+    db.collection('books')
+    .findOne({_id: ObjectId(req.params.id)})
+    .then(doc =>{
+        res.status(200).json(doc)
+    })
+    .catch(err =>{
+        res.status(500).json({error: 'not found'})
+    })
+})
+
+
+app.post('/books', (req, res) =>{
+  const book = req.body
+
+  db.collection('books')
+  .insertOne(book)
+  .then(result =>{
+    res.status(201).json(result)
+  })
+  .catch(err =>{
+    res.status(500).json({err:'coudnt do it sad'})
+  })
+})
+
+
+app.delete('/books/:id', (req, res) =>{
+  if(ObjectId.isValid(req.params.id)){
+    db.collection('books')
+    .deleteOne({_id: ObjectId(req.params.id)})
+    .then(result =>{
+      res.status(200).json(result)
+    })
+    .catch(err =>{
+      res.status(500).json({err: "not deleted"})
+    })
+  }else{
+    res.status(500).json({error: "not valid doc"})
+  }
+})
---
+++ mongodbNodejs/del.js
@@ -0,0 +1,79 @@
+[{
+    "title": "Create a product",
+    "description": "Create a new product"
+  },
+  {
+    "title": "Get product list",
+    "description": "Retreive the list of product."
+  },
+  {
+    "title": "Get Product Details",
+    "description": "Retrieve the details for the specific product"
+  },
+  {
+    "title": "Update Product",
+    "description": "Update the details for a specific product."
+  },
+  {
+    "title": "Create product variant",
+    "description": "Create a new product variant representing an option of the product version."
+  },
+  {
+    "title": "Get order details",
+    "description": "Retrieve the details of the specific order"
+  },{
+    "title": "Create product variant",
+    "description": "Create a new product variant representing an option of the product version."
+  },
+  {
+    "title": "Get order details",
+    "description": "Retrieve the details of the specific order"
+  },{
+       "title": "Get all orders",
+    "description":"Retrieve data of all the orders."
+  },{
+       "title": "Create price rule",
+    "description":"Create a rule for price of a product."
+  },{
+       "title": "Get price rules",
+    "description":"Retrieve all the pricing rules of all products."
+  },{
+       "title": "Get single price rule",
+    "description":"Retrieve pricing rule of a specific product."
+  },{
+       "title": "Delete price rule",
+    "description":"Delete pricing rule of a product."
+  },{
+       "title": "Create dicscount code",
+    "description":"Create new discount code for a product."
+  },{
+       "title": "Get all discount codes",
+    "description":"Retrieve all the dicount codes of all products."
+  },{
+       "title": "Get single discount codes",
+    "description":"Retrieve discount codes of a specific product."
+  },{
+       "title": "Delete discount code",
+    "description":"Delete discount codes of a product."
+  },{
+       "title": "Get all customers",
+    "description":"Retrieve list of all the customers."
+  },{
+       "title": "Get single customer",
+    "description":"Retrieve details of a specific customer."
+  },{
+       "title": "Retrieve orders by customer",
+    "description":"Get detsild of all the orders of a customer."
+  },{
+       "title": "Search customer by query",
+    "description":"Provide a query to find the eligible customer as per the query."
+  },{
+       "title": "Create customer",
+    "description":"Create a new customer."
+  },{
+       "title": "Update customer",
+    "description":"Update details of existing customer."
+  }
+
+
+]
